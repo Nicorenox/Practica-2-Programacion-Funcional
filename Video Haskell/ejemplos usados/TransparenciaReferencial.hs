c6f5c5f99640efd28bea6f7c01eab13b80171f1e
Nicorenox <100243743+Nicorenox@users.noreply.github.com>
2023-10-31 19:54:53 -0500

    Update TransparenciaReferencial.hs

--- Video Haskell/ejemplos usados/TransparenciaReferencial.hs
+++ Video Haskell/ejemplos usados/TransparenciaReferencial.hs
@@ -1,6 +1,10 @@
+-- Define la función square que toma un número entero 'x' y devuelve su cuadrado.
 square :: Int -> Int
 square x = x * x
 
--- Uso de la función square
-result1 = square 5
-result2 = 25
+main :: IO ()
+main = do
+  let result1 = square 5
+      result2 = 25
+  print result1
+  print result2
