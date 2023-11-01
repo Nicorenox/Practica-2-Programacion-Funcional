efb478fcf77e9256c9d930b711752d5c770a9a1b
Nicorenox <100243743+Nicorenox@users.noreply.github.com>
2023-10-31 19:55:25 -0500

    Update funcionesDePrimerOrden.hs

--- Video Haskell/ejemplos usados/funcionesDePrimerOrden.hs
+++ Video Haskell/ejemplos usados/funcionesDePrimerOrden.hs
@@ -1,8 +1,12 @@
+-- Define la función applyTwice que toma una función 'f' y un valor 'x', y aplica 'f' dos veces a 'x'.
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
 
--- Ejemplo de uso:
+-- Define una función 'multiplyByTwo' que toma un número entero 'x' y lo multiplica por 2.
 multiplyByTwo :: Int -> Int
 multiplyByTwo x = x * 2
 
-result = applyTwice multiplyByTwo 2
+main :: IO ()
+main = do
+  let result = applyTwice multiplyByTwo (2 :: Int)
+  print result
